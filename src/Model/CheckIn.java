6b913a5ec81d175906632239e3cd31e9b8c9576d
ahmedash11 <ahmedashh11@gmail.com>
2019-04-27 19:14:12 +0200

    Update CheckIn.java

--- src/Model/CheckIn.java
+++ src/Model/CheckIn.java
@@ -18,6 +18,7 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.MongoClientOptions;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
